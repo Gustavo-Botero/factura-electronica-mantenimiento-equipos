d99ed51190bac52faefb85d6d27b7594cc38af71
Gustavo Botero <gustavoadolfob92@gmail.com>
2021-05-20 20:59:27 -0500

    Creando RepositoryServiceProvider y configurando* app.php para que funcione

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            'App\Repositories\Contracts\Modulos\Maintenance\MaintenanceRepositoryInterface',
+            'App\Repositories\Modulos\Maintenance\MaintenanceRepository'
+        );
+
+        $this->app->bind(
+            'App\Repositories\Contracts\Modulos\Reference\ReferenceRepositoryInterface',
+            'App\Repositories\Modulos\Reference\ReferenceRepository'
+        );
+
+        $this->app->bind(
+            'App\Repositories\Contracts\Modulos\TypeTeam\TypeTeamRepositoryInterface',
+            'App\Repositories\Modulos\TypeTeam\TypeTeamRepository'
+        );
+
+        $this->app->bind(
+            'App\Repositories\Contracts\Modulos\TypeDocument\TypeDocumentRepositoryInterface',
+            'App\Repositories\Modulos\TypeDocument\TypeDocumentRepository'
+        );
+
+        $this->app->bind(
+            'App\Repositories\Contracts\Modulos\TypeMaintenance\TypeMaintenanceRepositoryInterface',
+            'App\Repositories\Modulos\TypeMaintenance\TypeMaintenanceRepository'
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
